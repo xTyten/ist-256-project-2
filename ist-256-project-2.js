319e3973d7e2f40a87b0eabe79edb6aafff9f53f
xTyten <tylerrenzejan@gmail.com>
2024-11-19 20:36:33 -0500

    Checkbox toggles

--- ist-256-project-2.js
+++ ist-256-project-2.js
@@ -22,40 +22,58 @@ export class Ist256Project2 extends DDDSuper(I18NMixin(LitElement)) {
 
   constructor() {
     super();
-    this.title = "";
-    this.t = this.t || {};
-    this.t = {
-      ...this.t,
-      title: "Title",
-    };
-    this.registerLocalization({
-      context: this,
-      localesPath:
-        new URL("./locales/ist-256-project-2.ar.json", import.meta.url).href +
-        "/../",
-      locales: ["ar", "es", "hi", "zh"],
-    });
+    this.accessories = "";  //0-9. 0 is nothing
+    this.base = "";         //0 or 5 (Male 0-4, Female 5-9)
+    this.leg = "";          //set to 0 and hide input
+    this.face = "";         //0-5. 0 is nothing
+    this.faceitem = "";     //0-9. 0 is nothing
+    this.hair = "";         //0-9
+    this.pants = "";        //0-9
+    this.shirt = "";        //0-9
+    this.skin = "";         //0-9
+    this.hatcolor = "";     //0-9
+    this.hat = "";          //15 hats including "none"
+    this.fire = false;      // true or false
+    this.walking = false;   // true or false
+    this.circle = false;    // true or false
   }
 
   // Lit reactive properties
   static get properties() {
     return {
-      ...super.properties,
-      title: { type: String },
+      accessories: { type: String, reflect: true }, //syncs property and attribute
+      base: { type: String, reflect: true },
+      leg: { type: String, reflect: true },
+      face: { type: String, reflect: true },
+      faceitem: { type: String, reflect: true },
+      hair: { type: String, reflect: true },
+      pants: { type: String, reflect: true },
+      shirt: { type: String, reflect: true },
+      skin: { type: String, reflect: true },
+      hatcolor: { type: String, reflect: true },
+      hat: { type: String, reflect: true },
+      fire: { type: Boolean, reflect: true },
+      walking: { type: Boolean, reflect: true },
+      circle: { type: Boolean, reflect: true },
     };
   }
 
   // Lit scoped styles
   static get styles() {
-    return [super.styles,
+    return [
     css`
       .entire-wrapper {
         display: flex;
         flex-direction: row;
+        flex-wrap: wrap;
       }
-      .character-seed-wrapper {
-        display: flex;
-        flex-direction: column;
+      td {
+        height: 40px;;
+        padding: var(--ddd-spacing-1); //4px
+        font-size: var(--ddd-font-size-xxs); //20px
+      }
+      .td-title {
+        font-weight: var(--ddd-font-weight-bold, 700); //bold font
       }
     `];
   }
@@ -65,15 +83,20 @@ export class Ist256Project2 extends DDDSuper(I18NMixin(LitElement)) {
     return html`
     <div class="entire-wrapper">
       <div class="character-seed-wrapper">
-        <rpg-character width="500" height="500">
-        
+        <rpg-character 
+          width="500" 
+          height="500"
+          ?fire=${this.fire}
+          ?walking=${this.walking}
+          ?circle=${this.circle}
+          >
         </rpg-character>
         <h3>Seed: </h3>
       </div>
       <table>
         <form>
           <tr>
-            <td>Accessories:</td>
+            <td class="td-title">Accessories:</td>
             <td>
               <wired-combo selected="zero" role="combobox" aria-haspopup="listbox" tabindex="0" class="wired-rendered" aria-expanded="false">
                 <wired-item value="zero" role="option" class="wired-rendered">0</wired-item>
@@ -91,7 +114,56 @@ export class Ist256Project2 extends DDDSuper(I18NMixin(LitElement)) {
           </tr>
           <tr>
             <!-- Bases 0-4 are male. Bases 5-9 are female -->
-            <td>Base:</td> 
+            <td class="td-title">Base:</td> 
+            <td>
+              <wired-combo selected="zero" role="combobox" aria-haspopup="listbox" tabindex="0" class="wired-rendered" aria-expanded="false">
+                <wired-item value="zero" role="option" class="wired-rendered">0</wired-item>
+                <wired-item value="one" role="option" class="wired-rendered">1</wired-item>
+                <wired-item value="two" role="option" class="wired-rendered">2</wired-item>
+                <wired-item value="three" role="option" class="wired-rendered">3</wired-item>
+                <wired-item value="four" role="option" class="wired-rendered">4</wired-item>
+                <wired-item value="five" role="option" class="wired-rendered">5</wired-item>
+                <wired-item value="six" role="option" class="wired-rendered">6</wired-item>
+                <wired-item value="seven" role="option" class="wired-rendered">7</wired-item>
+                <wired-item value="eight" role="option" class="wired-rendered">8</wired-item>
+                <wired-item value="nine" role="option" class="wired-rendered">9</wired-item>
+              </wired-combo>
+            </td>
+          </tr>
+          <!--
+          <tr>
+            <td class="td-title">Leg:</td>
+            <td>
+              <wired-combo selected="zero" role="combobox" aria-haspopup="listbox" tabindex="0" class="wired-rendered" aria-expanded="false">
+                <wired-item value="zero" role="option" class="wired-rendered">0</wired-item>
+                <wired-item value="one" role="option" class="wired-rendered">1</wired-item>
+                <wired-item value="two" role="option" class="wired-rendered">2</wired-item>
+                <wired-item value="three" role="option" class="wired-rendered">3</wired-item>
+                <wired-item value="four" role="option" class="wired-rendered">4</wired-item>
+                <wired-item value="five" role="option" class="wired-rendered">5</wired-item>
+                <wired-item value="six" role="option" class="wired-rendered">6</wired-item>
+                <wired-item value="seven" role="option" class="wired-rendered">7</wired-item>
+                <wired-item value="eight" role="option" class="wired-rendered">8</wired-item>
+                <wired-item value="nine" role="option" class="wired-rendered">9</wired-item>
+              </wired-combo>
+            </td>
+          </tr>
+          -->
+          <tr>
+            <td class="td-title">Face:</td>
+            <td>
+              <wired-combo selected="zero" role="combobox" aria-haspopup="listbox" tabindex="0" class="wired-rendered" aria-expanded="false">
+                <wired-item value="zero" role="option" class="wired-rendered">0</wired-item>
+                <wired-item value="one" role="option" class="wired-rendered">1</wired-item>
+                <wired-item value="two" role="option" class="wired-rendered">2</wired-item>
+                <wired-item value="three" role="option" class="wired-rendered">3</wired-item>
+                <wired-item value="four" role="option" class="wired-rendered">4</wired-item>
+                <wired-item value="five" role="option" class="wired-rendered">5</wired-item>
+              </wired-combo>
+            </td>
+          </tr>
+          <tr>
+            <td class="td-title">Face Item:</td>
             <td>
               <wired-combo selected="zero" role="combobox" aria-haspopup="listbox" tabindex="0" class="wired-rendered" aria-expanded="false">
                 <wired-item value="zero" role="option" class="wired-rendered">0</wired-item>
@@ -108,8 +180,7 @@ export class Ist256Project2 extends DDDSuper(I18NMixin(LitElement)) {
             </td>
           </tr>
           <tr>
-            <!-- ignored, set to 0 and disable / hide input -->
-            <td>Leg:</td>
+            <td class="td-title">Hair:</td>
             <td>
               <wired-combo selected="zero" role="combobox" aria-haspopup="listbox" tabindex="0" class="wired-rendered" aria-expanded="false">
                 <wired-item value="zero" role="option" class="wired-rendered">0</wired-item>
@@ -126,20 +197,7 @@ export class Ist256Project2 extends DDDSuper(I18NMixin(LitElement)) {
             </td>
           </tr>
           <tr>
-            <td>Face:</td>
-            <td>
-              <wired-combo selected="zero" role="combobox" aria-haspopup="listbox" tabindex="0" class="wired-rendered" aria-expanded="false">
-                <wired-item value="zero" role="option" class="wired-rendered">0</wired-item>
-                <wired-item value="one" role="option" class="wired-rendered">1</wired-item>
-                <wired-item value="two" role="option" class="wired-rendered">2</wired-item>
-                <wired-item value="three" role="option" class="wired-rendered">3</wired-item>
-                <wired-item value="four" role="option" class="wired-rendered">4</wired-item>
-                <wired-item value="five" role="option" class="wired-rendered">5</wired-item>
-              </wired-combo>
-            </td>
-          </tr>
-          <tr>
-            <td>Face Item:</td>
+            <td class="td-title">Pants:</td>
             <td>
               <wired-combo selected="zero" role="combobox" aria-haspopup="listbox" tabindex="0" class="wired-rendered" aria-expanded="false">
                 <wired-item value="zero" role="option" class="wired-rendered">0</wired-item>
@@ -156,7 +214,7 @@ export class Ist256Project2 extends DDDSuper(I18NMixin(LitElement)) {
             </td>
           </tr>
           <tr>
-            <td>Hair:</td>
+            <td class="td-title">Shirt:</td>
             <td>
               <wired-combo selected="zero" role="combobox" aria-haspopup="listbox" tabindex="0" class="wired-rendered" aria-expanded="false">
                 <wired-item value="zero" role="option" class="wired-rendered">0</wired-item>
@@ -173,7 +231,7 @@ export class Ist256Project2 extends DDDSuper(I18NMixin(LitElement)) {
             </td>
           </tr>
           <tr>
-            <td>Pants:</td>
+            <td class="td-title">Skin:</td>
             <td>
               <wired-combo selected="zero" role="combobox" aria-haspopup="listbox" tabindex="0" class="wired-rendered" aria-expanded="false">
                 <wired-item value="zero" role="option" class="wired-rendered">0</wired-item>
@@ -190,7 +248,7 @@ export class Ist256Project2 extends DDDSuper(I18NMixin(LitElement)) {
             </td>
           </tr>
           <tr>
-            <td>Shirt:</td>
+            <td class="td-title">Hat Color:</td>
             <td>
               <wired-combo selected="zero" role="combobox" aria-haspopup="listbox" tabindex="0" class="wired-rendered" aria-expanded="false">
                 <wired-item value="zero" role="option" class="wired-rendered">0</wired-item>
@@ -207,41 +265,7 @@ export class Ist256Project2 extends DDDSuper(I18NMixin(LitElement)) {
             </td>
           </tr>
           <tr>
-            <td>Skin:</td>
-            <td>
-              <wired-combo selected="zero" role="combobox" aria-haspopup="listbox" tabindex="0" class="wired-rendered" aria-expanded="false">
-                <wired-item value="zero" role="option" class="wired-rendered">0</wired-item>
-                <wired-item value="one" role="option" class="wired-rendered">1</wired-item>
-                <wired-item value="two" role="option" class="wired-rendered">2</wired-item>
-                <wired-item value="three" role="option" class="wired-rendered">3</wired-item>
-                <wired-item value="four" role="option" class="wired-rendered">4</wired-item>
-                <wired-item value="five" role="option" class="wired-rendered">5</wired-item>
-                <wired-item value="six" role="option" class="wired-rendered">6</wired-item>
-                <wired-item value="seven" role="option" class="wired-rendered">7</wired-item>
-                <wired-item value="eight" role="option" class="wired-rendered">8</wired-item>
-                <wired-item value="nine" role="option" class="wired-rendered">9</wired-item>
-              </wired-combo>
-            </td>
-          </tr>
-          <tr>
-            <td>Hat Color:</td>
-            <td>
-              <wired-combo selected="zero" role="combobox" aria-haspopup="listbox" tabindex="0" class="wired-rendered" aria-expanded="false">
-                <wired-item value="zero" role="option" class="wired-rendered">0</wired-item>
-                <wired-item value="one" role="option" class="wired-rendered">1</wired-item>
-                <wired-item value="two" role="option" class="wired-rendered">2</wired-item>
-                <wired-item value="three" role="option" class="wired-rendered">3</wired-item>
-                <wired-item value="four" role="option" class="wired-rendered">4</wired-item>
-                <wired-item value="five" role="option" class="wired-rendered">5</wired-item>
-                <wired-item value="six" role="option" class="wired-rendered">6</wired-item>
-                <wired-item value="seven" role="option" class="wired-rendered">7</wired-item>
-                <wired-item value="eight" role="option" class="wired-rendered">8</wired-item>
-                <wired-item value="nine" role="option" class="wired-rendered">9</wired-item>
-              </wired-combo>
-            </td>
-          </tr>
-          <tr>
-            <td>Hat:</td>
+            <td class="td-title">Hat:</td>
             <td>
               <wired-combo selected="none" role="combobox" aria-haspopup="listbox" tabindex="0" class="wired-rendered" aria-expanded="false">
                 <wired-item value="none" role="option" class="wired-rendered">none</wired-item>
@@ -259,19 +283,19 @@ export class Ist256Project2 extends DDDSuper(I18NMixin(LitElement)) {
             </td>
           </tr>
           <tr>
-            <td>On fire:</td>
-            <td><wired-checkbox class="wired-rendered"></wired-checkbox></td>
+            <td class="td-title"}>On fire:</td>
+            <td><wired-checkbox class="wired-rendered" @change=${this.toggleFire}></wired-checkbox></td>
           </tr>
           <tr>
-            <td>Walking:</td>
-            <td><wired-checkbox class="wired-rendered"></wired-checkbox></td>
+            <td class="td-title">Walking:</td>
+            <td><wired-checkbox class="wired-rendered" @change=${this.toggleWalking}></wired-checkbox></td>
           </tr>
           <tr>
-            <td>Circle:</td>
-            <td><wired-checkbox class="wired-rendered"></wired-checkbox></td>
+            <td class="td-title">Circle:</td>
+            <td><wired-checkbox class="wired-rendered" @change=${this.toggleCircle}></wired-checkbox></td>
           </tr>
           <tr>
-            <td><wired-button>Share Link</wired-button></td>
+            <td><wired-button @click=${this.testButton}>Share Link</wired-button></td>
           </tr>
         </form>
       </table>
@@ -279,6 +303,18 @@ export class Ist256Project2 extends DDDSuper(I18NMixin(LitElement)) {
     `;
   }
 
+  toggleFire() {
+    this.fire = !this.fire;
+  }
+  toggleWalking() {
+    this.walking = !this.walking;
+  }
+  toggleCircle() {
+    this.circle = !this.circle;
+  }
+  testButton() {
+    window.alert(`Button pressed`);
+  }
   /**
    * haxProperties integration via file reference
    */
